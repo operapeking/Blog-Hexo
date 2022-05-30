3cc9f6d99b8409fe945d25c4e3610393e75c4acf
pekingopera <PekingOpera@protonmail.com>
2022-05-30 17:08:58 +0800

    修复主题

---
+++ scripts/injector.js
@@ -0,0 +1,1 @@
+hexo.extend.injector.register('body_end', `<script src="/js/togh.js"></script>`, 'post');
---
+++ source/js/togh.js
@@ -0,0 +1,8 @@
+(function () {
+    var url = window.location.pathname;
+    url = url.slice(0, url.length - 1);
+    var filename = "https://github.com/operapeking/blog-hexo/blob/master/source/_posts/" + url.substring(url.lastIndexOf('/') + 1, url.length) + ".md";
+    var posts = document.getElementsByClassName('post-content');
+    var insert = '<a href="' + filename + '" target="_blank">在 Github 中查看</a>';
+    posts[0].insertAdjacentHTML('afterbegin', insert);
+})();

83827c0ef9d2226719ab83dcda2b9b564819bcbf
operapeking <pekingopera@protonmail.com>
2022-07-22 21:18:44 +0800

    优化注入

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,1 +1,2 @@
 hexo.extend.injector.register('body_end', `<script src="/js/togh.js"></script>`, 'post');
+hexo.extend.injector.register('body_end', `<script src="/js/tocn.js"></script>`, 'post');
--- source/js/togh.js
+++ source/js/togh.js
@@ -4,5 +4,5 @@
     var filename = "https://github.com/operapeking/blog-hexo/blob/master/source/_posts/" + url.substring(url.lastIndexOf('/') + 1, url.length) + ".md";
     var posts = document.getElementsByClassName('post-content');
     var insert = '<a href="' + filename + '" target="_blank">在 Github 中查看</a>';
-    posts[0].insertAdjacentHTML('afterbegin', insert);
+    posts[0].insertAdjacentHTML('beforeend', insert);
 })();

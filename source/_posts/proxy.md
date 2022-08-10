1ca29ac03a5e06251072c9845e2f6a46d93f6063
operapeking <pekingopera@protonmail.com>
2022-08-10 11:01:17 +0800

    修复 bug

--- source/_posts/proxy.md
+++ source/_posts/proxy.md
@@ -13,8 +13,8 @@ tags:
 
 git
 ```bash
+git config --global http.proxy 'http://127.0.0.1:10809'
 git config --global https.proxy 'http://127.0.0.1:10809'
-git config --global https.proxy 'https://127.0.0.1:10809'
 ```
 
 npm
--- source/js/tocn.js
+++ source/js/tocn.js
@@ -1,6 +1,6 @@
 (function () {
     var url = window.location.pathname;
-    url = 'https://pekingopera.ddns.net:83' + url;
+    url = 'https://masterd.tk:83' + url;
     var posts = document.getElementsByClassName('post-content');
     var insert = '<a href="' + url + '">在镜像网站中查看</a><br><br>';
     posts[0].insertAdjacentHTML('afterbegin', insert);
